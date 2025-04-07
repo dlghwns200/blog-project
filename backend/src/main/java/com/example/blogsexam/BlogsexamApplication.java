905ebd8475f3150937b0397081003fc8bc44481b
dlghwns200 <dlghwns200@gmail.com>
2025-04-07 18:54:30 +0900

    첫번쨰

---
+++ backend/src/main/java/com/example/blogsexam/BlogsexamApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogsexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsexamApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/blogsexam/BlogsexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogsexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
